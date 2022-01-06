4a2efeac66d34cdbb1316d4c031da14b23d6fff8
zbedkowska@student.agh.edu.pl <zbedkowska@student.agh.edu.pl>
2022-01-06 13:58:50 +0100

    Dodanie geterów, seterów i Date.h do user

--- User.h
+++ User.h
@@ -1,15 +1,9 @@
 #include <string>
+#include "Date.h"
 using namespace std;
 
 #ifndef USER_H
 #define USER_H
-
-struct Date
-	{
-	int day;
-	int month;
-	int year;
-	};
 
 class User {
 
@@ -25,6 +19,8 @@ private:
 	static int usersCount;
 
 public:
+	User();
+
 	void addReservation(int bookId);
 
 	void removeReservation(int reservationId);
@@ -36,6 +32,27 @@ public:
 	void removeBorrowment();
 
 	void printBorrowments();
+
+	void setName(string& newName);
+
+	void setId(int newId);
+
+	void setPassword(string& newPassword);
+
+	void setFine(double newFine);
+
+	void setBirthDate(Date newBirthDate);
+
+	string getName();
+
+	int getId();
+
+	string getPassword();
+
+	double getFine();
+
+	Date getBirthDate();
+
 };
 
 #endif
