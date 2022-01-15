75822596d34742a3397c3fa9a7099da73b0f4be4
ZuzaBedkowska <zbedkowska@student.agh.edu.pl>
2022-01-15 17:39:41 +0100

    dodanie pliku userTests

---
+++ TDD/UserTests.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "gtest/gtest.h"
+#include "../Book.h"
+#include "../Date.h"
+#include "../Reservation.h"
+#include "../User.h"
+#include "../Item.h"
+
+
+TEST(checkUserTest, UserTest) //test jednostkowy do userTest
+{
+	User u_test("a", "b", { 1,1,1900 });
+	Date d_test(-1, -1, -1);
+	Book b_test;
+	EXPECT_EQ(u_test.userTest(), true); //0 wypozyczonych ksiazek
+	u_test.addReservation(b_test);
+	//EXPECT_EQ(u_test.userTest(), true); //1 wypozyczona ksiazka
+	/*u_test.addReservation(b_test);
+	EXPECT_EQ(u_test.userTest(), true);
+	u_test.addReservation(b_test);
+	EXPECT_EQ(u_test.userTest(), true);
+	u_test.addReservation(b_test);
+	EXPECT_EQ(u_test.userTest(), true);
+	u_test.addReservation(b_test);
+	EXPECT_EQ(u_test.userTest(), false);*/
+}
